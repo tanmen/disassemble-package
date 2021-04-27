b973e1d7491e0915c6aef5ba8197d42ff489a780
tanmen <yt.prog@gmail.com>
2021-04-28 07:21:09 +0900

    fix: semantic-release hadn't been called.

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,3 +1,10 @@
+import {
+  disassembleBabel,
+  disassembleBrowserslist,
+  disassembleEslint,
+  disassembleHusky,
+  disassembleJest, disassembleSemanticRelease
+} from "../disassemblers";
 import {DisassemblePackage} from "../index";
 
 jest.mock('fs/promises')
@@ -5,7 +12,13 @@ jest.mock('../disassemblers')
 
 describe('nominal', () => {
   it('should be run', async () => {
-
     await DisassemblePackage('test', {space: 5})
+
+    expect(disassembleBabel).toBeCalled()
+    expect(disassembleBrowserslist).toBeCalled()
+    expect(disassembleEslint).toBeCalled()
+    expect(disassembleHusky).toBeCalled()
+    expect(disassembleJest).toBeCalled()
+    expect(disassembleSemanticRelease).toBeCalled()
   });
 });
