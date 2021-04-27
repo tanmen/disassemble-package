9fe3f5489ebbd02e9bd06278f2509f10c0ccd6f6
tanmen <yt.prog@gmail.com>
2021-04-28 07:22:18 +0900

    fix: changed the output to gorgeous.

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
+import chalk from "chalk";
 import commandLineArgs from "command-line-args";
 import ora from "ora";
 import {DisassemblePackage} from "./index";
+
+console.log(`${chalk.white('»')}  ${chalk.gray(chalk`${require('../package.json').name} {white v${require('../package.json').version}}`)}`)
 
 const mainDefinitions = [
   {name: 'path', type: String, defaultOption: true}
@@ -16,4 +19,7 @@ const spinner = ora('Disassembling');
 
 spinner.start()
 DisassemblePackage(option.path, {space: option.space})
-  .finally(() => spinner.stop())
+  .finally(() => {
+    spinner.stop()
+    console.log(`${chalk.green('✔')} ${chalk.white('disassembled')}`)
+  })
